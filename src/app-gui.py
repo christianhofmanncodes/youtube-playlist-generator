1446f55bae5a08700f92af06830226541f31f204
christianhofmanncodes <christianhofmann@mailbox.org>
2022-03-19 15:47:16 +0100

    change ui design with qt_material and add first functions to buttons

--- src/app-gui.py
+++ src/app-gui.py
@@ -1,10 +1,44 @@
 import sys
-from PyQt6 import QtWidgets, uic
+from PyQt6 import uic
+from PyQt6.QtWidgets import (
+    QMainWindow,
+    QFileDialog,
+    QApplication,
+    QWidget,
+    QPushButton,
+    QVBoxLayout,
+)
+from qt_material import apply_stylesheet
 
 
-app = QtWidgets.QApplication(sys.argv)
+class Ui(QMainWindow):
+    def __init__(self):
+        super(Ui, self).__init__()
+        uic.loadUi("src/gui/form.ui", self)
+        self.pushButton_add.clicked.connect(self.addButtonPressed)
+        self.pushButton_copy.clicked.connect(self.copyButtonPressed)
 
-window = uic.loadUi("src/gui/form.ui")
-window.show()
+    def addButtonPressed(self):
+        self.textEdit_url_id.setText("Button 'Add' clicked!")
 
-app.exec()
+    def copyButtonPressed(self):
+        self.textEdit_playlist_generated_url.setText("Button 'Copy' clicked!")
+
+
+def main():
+    app = QApplication(sys.argv)
+    apply_stylesheet(app, theme="dark_red.xml")
+    window = Ui()
+    window.show()  # show window
+    sys.exit(app.exec())
+
+    # app = QtWidgets.QApplication(sys.argv)
+    # window = uic.loadUi("src/gui/form.ui")
+    # apply_stylesheet(app, theme="dark_red.xml")
+    # window.show()
+
+    # app.exec()
+
+
+if __name__ == "__main__":
+    main()
