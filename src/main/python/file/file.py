33e37d7eaaeb2cec7a4c0b4ddf80811278515960
christianhofmanncodes <christianhofmann@mailbox.org>
2022-09-08 16:16:16 +0200

    Add exceptions to read_json_file(), add new function check_file_format()

--- src/main/python/file/file.py
+++ src/main/python/file/file.py
@@ -1,13 +1,21 @@
 """file.file module"""
 
 import json
+import logging
 
 
 def read_json_file(filename: str) -> dict:
     """Return content from json-file."""
-    with open(filename, "r", encoding="UTF-8") as file:
-        data = json.load(file)
-    return data
+    try:
+        with open(filename, "r", encoding="UTF-8") as file:
+            data = json.load(file)
+        return data
+    except json.decoder.JSONDecodeError:
+        logging.error("JSON file empty!")
+        return ""
+    except FileNotFoundError:
+        logging.error("File %s not found!", filename)
+        return ""
 
 
 def read_file(filename: str) -> str:
@@ -21,3 +29,9 @@ def write_json_file(filename: str, content: dict) -> None:
     """Write content into json file by given filename."""
     with open(filename, "w", encoding="UTF-8") as file:
         json.dump(content, file, indent=4)
+
+
+def check_file_format(filename: str, file_format: str) -> bool:
+    """Check if file_format in filename and return bool."""
+    if file_format in filename:
+        return True
