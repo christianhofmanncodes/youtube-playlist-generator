a3f2b83b68f560e9954400cce4b9bac6f045ce6f
christianhofmanncodes <christianhofmann@mailbox.org>
2022-09-12 22:53:29 +0200

    Add new shortcuts to settings_dialog

--- src/main/python/dialogs/settings_dialog.py
+++ src/main/python/dialogs/settings_dialog.py
@@ -46,29 +46,55 @@ class SettingsDialog(QDialog):
         self.pushButton_change_option5.clicked.connect(
             self.change_button_option5_clicked
         )
+        self.pushButton_change_option6.clicked.connect(
+            self.change_button_option6_clicked
+        )
+        self.pushButton_change_option7.clicked.connect(
+            self.change_button_option7_clicked
+        )
+        self.pushButton_change_option8.clicked.connect(
+            self.change_button_option8_clicked
+        )
+        self.pushButton_change_option9.clicked.connect(
+            self.change_button_option9_clicked
+        )
+        self.pushButton_change_option10.clicked.connect(
+            self.change_button_option10_clicked
+        )
+        self.pushButton_change_option11.clicked.connect(
+            self.change_button_option11_clicked
+        )
+        self.pushButton_change_option12.clicked.connect(
+            self.change_button_option12_clicked
+        )
 
     def load_settings(self, settings_dict: dict) -> None:  # doesn't work yet
         """Display settings in dialog from dict."""
         open_url_automatically = settings_dict["general"][0]["openURLautomatically"]
         copy_url_to_clipboard = settings_dict["general"][0]["copyURLtoClipboard"]
         program_language = settings_dict["general"][0]["programLanguage"]
-        # app_theme = settings_dict["general"][0]["appTheme"]
 
-        shortcut_import_new_playlist = settings_dict["keyboard_shortcuts"][0][
-            "importNewPlaylist"
-        ]
-        shortcut_export_playlist = settings_dict["keyboard_shortcuts"][0][
-            "exportPlaylist"
-        ]
-        shortcut_reset_playlist = settings_dict["keyboard_shortcuts"][0][
-            "clearPlaylist"
-        ]
-        shortcut_generate_playlist = settings_dict["keyboard_shortcuts"][0][
-            "generatePlaylist"
-        ]
+        shortcut_new_playlist = settings_dict["keyboard_shortcuts"][0]["newPlaylist"]
+        shortcut_open_playlist = settings_dict["keyboard_shortcuts"][0]["openPlaylist"]
+        shortcut_save_playlist = settings_dict["keyboard_shortcuts"][0]["savePlaylist"]
+        shortcut_add_item = settings_dict["keyboard_shortcuts"][0]["addItem"]
+        shortcut_delete_item = settings_dict["keyboard_shortcuts"][0]["deleteItem"]
+        shortcut_rename_item = settings_dict["keyboard_shortcuts"][0]["renameItem"]
         shortcut_shuffle_playlist = settings_dict["keyboard_shortcuts"][0][
             "shufflePlaylist"
         ]
+
+        shortcut_generate_playlist = settings_dict["keyboard_shortcuts"][0][
+            "generatePlaylist"
+        ]
+        shortcut_count_items = settings_dict["keyboard_shortcuts"][0]["countItems"]
+        shortcut_clear_all_items = settings_dict["keyboard_shortcuts"][0][
+            "clearAllItems"
+        ]
+        shortcut_remove_duplicates = settings_dict["keyboard_shortcuts"][0][
+            "removeDuplicates"
+        ]
+        shortcut_copy_url = settings_dict["keyboard_shortcuts"][0]["copyURL"]
 
         self.comboBox_language.setCurrentText(program_language)
 
@@ -84,11 +110,18 @@ class SettingsDialog(QDialog):
         elif copy_url_to_clipboard is False:
             self.checkBox_option2.setCheckState(Qt.CheckState.Unchecked)
 
-        self.label_keyboard_shortcuts_option1.setText(shortcut_import_new_playlist)
-        self.label_keyboard_shortcuts_option2.setText(shortcut_export_playlist)
-        self.label_keyboard_shortcuts_option3.setText(shortcut_reset_playlist)
-        self.label_keyboard_shortcuts_option4.setText(shortcut_generate_playlist)
-        self.label_keyboard_shortcuts_option5.setText(shortcut_shuffle_playlist)
+        self.label_keyboard_shortcuts_option1.setText(shortcut_new_playlist)
+        self.label_keyboard_shortcuts_option2.setText(shortcut_open_playlist)
+        self.label_keyboard_shortcuts_option3.setText(shortcut_save_playlist)
+        self.label_keyboard_shortcuts_option4.setText(shortcut_add_item)
+        self.label_keyboard_shortcuts_option5.setText(shortcut_delete_item)
+        self.label_keyboard_shortcuts_option6.setText(shortcut_rename_item)
+        self.label_keyboard_shortcuts_option7.setText(shortcut_shuffle_playlist)
+        self.label_keyboard_shortcuts_option8.setText(shortcut_generate_playlist)
+        self.label_keyboard_shortcuts_option9.setText(shortcut_count_items)
+        self.label_keyboard_shortcuts_option10.setText(shortcut_clear_all_items)
+        self.label_keyboard_shortcuts_option11.setText(shortcut_remove_duplicates)
+        self.label_keyboard_shortcuts_option12.setText(shortcut_copy_url)
 
     def change_theme(self) -> None:
         """Change theme in runtime."""
@@ -122,36 +155,85 @@ class SettingsDialog(QDialog):
         logging.debug("This will change the language...")
 
     def change_button_option1_clicked(self) -> None:
-        """Get text from keySequenceEdit1 field and display in label."""
-        if self.keySequenceEdit_option1.keySequence().toString() != "":
+        """Get text from keySequenceEdit field and display in label."""
+        if self.keySequenceEdit.keySequence().toString() != "":
             self.label_keyboard_shortcuts_option1.setText(
-                self.keySequenceEdit_option1.keySequence().toString()
+                self.keySequenceEdit.keySequence().toString()
             )
 
     def change_button_option2_clicked(self) -> None:
-        """Get text from keySequenceEdit2 field and display in label."""
-        if self.keySequenceEdit_option2.keySequence().toString() != "":
+        """Get text from keySequenceEdit field and display in label."""
+        if self.keySequenceEdit.keySequence().toString() != "":
             self.label_keyboard_shortcuts_option2.setText(
-                self.keySequenceEdit_option2.keySequence().toString()
+                self.keySequenceEdit.keySequence().toString()
             )
 
     def change_button_option3_clicked(self) -> None:
-        """Get text from keySequenceEdit3 field and display in label."""
-        if self.keySequenceEdit_option3.keySequence().toString() != "":
+        """Get text from keySequenceEdit field and display in label."""
+        if self.keySequenceEdit.keySequence().toString() != "":
             self.label_keyboard_shortcuts_option3.setText(
-                self.keySequenceEdit_option3.keySequence().toString()
+                self.keySequenceEdit.keySequence().toString()
             )
 
     def change_button_option4_clicked(self) -> None:
-        """Get text from keySequenceEdit4 field and display in label."""
-        if self.keySequenceEdit_option4.keySequence().toString() != "":
+        """Get text from keySequenceEdit field and display in label."""
+        if self.keySequenceEdit.keySequence().toString() != "":
             self.label_keyboard_shortcuts_option4.setText(
-                self.keySequenceEdit_option4.keySequence().toString()
+                self.keySequenceEdit.keySequence().toString()
             )
 
     def change_button_option5_clicked(self) -> None:
-        """Get text from keySequenceEdit5 field and display in label."""
-        if self.keySequenceEdit_option5.keySequence().toString() != "":
+        """Get text from keySequenceEdit field and display in label."""
+        if self.keySequenceEdit.keySequence().toString() != "":
             self.label_keyboard_shortcuts_option5.setText(
-                self.keySequenceEdit_option5.keySequence().toString()
+                self.keySequenceEdit.keySequence().toString()
+            )
+
+    def change_button_option6_clicked(self) -> None:
+        """Get text from keySequenceEdit field and display in label."""
+        if self.keySequenceEdit.keySequence().toString() != "":
+            self.label_keyboard_shortcuts_option6.setText(
+                self.keySequenceEdit.keySequence().toString()
+            )
+
+    def change_button_option7_clicked(self) -> None:
+        """Get text from keySequenceEdit field and display in label."""
+        if self.keySequenceEdit.keySequence().toString() != "":
+            self.label_keyboard_shortcuts_option7.setText(
+                self.keySequenceEdit.keySequence().toString()
+            )
+
+    def change_button_option8_clicked(self) -> None:
+        """Get text from keySequenceEdit field and display in label."""
+        if self.keySequenceEdit.keySequence().toString() != "":
+            self.label_keyboard_shortcuts_option8.setText(
+                self.keySequenceEdit.keySequence().toString()
+            )
+
+    def change_button_option9_clicked(self) -> None:
+        """Get text from keySequenceEdit field and display in label."""
+        if self.keySequenceEdit.keySequence().toString() != "":
+            self.label_keyboard_shortcuts_option9.setText(
+                self.keySequenceEdit.keySequence().toString()
+            )
+
+    def change_button_option10_clicked(self) -> None:
+        """Get text from keySequenceEdit field and display in label."""
+        if self.keySequenceEdit.keySequence().toString() != "":
+            self.label_keyboard_shortcuts_option10.setText(
+                self.keySequenceEdit.keySequence().toString()
+            )
+
+    def change_button_option11_clicked(self) -> None:
+        """Get text from keySequenceEdit field and display in label."""
+        if self.keySequenceEdit.keySequence().toString() != "":
+            self.label_keyboard_shortcuts_option11.setText(
+                self.keySequenceEdit.keySequence().toString()
+            )
+
+    def change_button_option12_clicked(self) -> None:
+        """Get text from keySequenceEdit field and display in label."""
+        if self.keySequenceEdit.keySequence().toString() != "":
+            self.label_keyboard_shortcuts_option12.setText(
+                self.keySequenceEdit.keySequence().toString()
             )
