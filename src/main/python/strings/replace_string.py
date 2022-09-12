8091fecc49025dc1db76e373aa9f04a63c0e4ef9
christianhofmanncodes <christianhofmann@mailbox.org>
2022-09-12 22:52:09 +0200

    Add create_comma_separated_string to module strings.replace_string

--- src/main/python/strings/replace_string.py
+++ src/main/python/strings/replace_string.py
@@ -4,3 +4,8 @@
 def replace_space_in_string(string: str) -> str:
     """Return string with spaces replaced by %20."""
     return string.replace(" ", "%20")
+
+
+def create_comma_separated_string(content_list: list) -> str:
+    """Add commas after each item from list and return it as a string."""
+    return ",".join(content_list)
