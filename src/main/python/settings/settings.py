d40fa9d9d7f11932ae98756ebc1b2ba2a5672840
christianhofmanncodes <christianhofmann@mailbox.org>
2022-09-15 23:47:41 +0200

    Remove contextlib action, file is only for constants now

--- src/main/python/settings/settings.py
+++ src/main/python/settings/settings.py
@@ -1,15 +1,5 @@
 """module settings.settings"""
 
-import contextlib
-
-
 APP_VERSION = "0.1.0"
 RELEASE_DATE = "Sept 30 2022"
 RECENT_FILES_STRING = "Clear recent files"
-
-
-with contextlib.suppress(ImportError):
-    from ctypes import windll  # Only exists on Windows
-
-    APP_ID = f"christianhofmann.youtube-playlist-generator.gui.{APP_VERSION}"
-    windll.shell32.SetCurrentProcessExplicitAppUserModelID(APP_ID)
