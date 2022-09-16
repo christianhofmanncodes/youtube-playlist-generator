595ce8cc6314a025e6af75227ca8c734a58c02be
christianhofmanncodes <christianhofmann@mailbox.org>
2022-09-16 16:10:12 +0200

    Add constants SETTING_FILE_LOCATION, DEFAULT_SETTINGS_FILE_LOCATION

--- src/main/python/settings/settings.py
+++ src/main/python/settings/settings.py
@@ -3,3 +3,5 @@
 APP_VERSION = "0.1.0"
 RELEASE_DATE = "Sept 30 2022"
 RECENT_FILES_STRING = "Clear recent files"
+SETTING_FILE_LOCATION = "config/settings.config"
+DEFAULT_SETTINGS_FILE_LOCATION = "config/settings_default.config"
