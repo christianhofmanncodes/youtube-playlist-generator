7ca8a7f27a839e891f26b05f334143d9d911635b
christianhofmanncodes <christianhofmann@mailbox.org>
2022-09-20 15:40:22 +0200

    Add new constant "APP_ICON"

--- src/main/python/settings/settings.py
+++ src/main/python/settings/settings.py
@@ -1,6 +1,7 @@
 """module settings.settings"""
 
 APP_VERSION = "0.1.0"
+APP_ICON = "icon/youtube-play.icns"
 RELEASE_DATE = "Sept 30 2022"
 RECENT_FILES_STRING = "Clear recent files"
 SETTING_FILE_LOCATION = "config/settings.config"
