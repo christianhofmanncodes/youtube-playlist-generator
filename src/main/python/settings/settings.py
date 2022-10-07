b8581b2fa44341da9ea2665861ea300f173ab69b
christianhofmanncodes <christianhofmann@mailbox.org>
2022-10-07 19:56:19 +0200

    Change release date

--- src/main/python/settings/settings.py
+++ src/main/python/settings/settings.py
@@ -2,7 +2,7 @@
 
 APP_VERSION = "0.1.0"
 APP_ICON = "icon/youtube-play.icns"
-RELEASE_DATE = "Sept 30 2022"
+RELEASE_DATE = "Oct 30 2022"
 RECENT_FILES_STRING = "Clear recent files"
 SETTING_FILE_LOCATION = "config/settings.config"
 DEFAULT_SETTINGS_FILE_LOCATION = "config/settings_default.config"
