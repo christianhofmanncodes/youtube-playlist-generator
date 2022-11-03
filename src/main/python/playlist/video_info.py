8b489b1ac3d600b4dc7318e626dae4dff6d2221a
christianhofmanncodes <christianhofmann@mailbox.org>
2022-11-03 18:14:03 +0100

    Remove old functions, replace by new function get_video_info(), use new package pytube for scraping

--- src/main/python/playlist/video_info.py
+++ src/main/python/playlist/video_info.py
@@ -1,56 +1,37 @@
 """module playlist.video_info"""
 
-import urllib
+import logging
 
-from bs4 import BeautifulSoup
-from lxml import etree
+from pytube import YouTube, exceptions
 
 
-def get_title_channel_from_youtube_link(video_id) -> str:
+def get_video_info(video_id: str) -> dict[str]:
     """
-    The get_title_channel_from_youtube_link function takes a YouTube video ID
-    as an argument and returns the title of the video.
+    The get_video_info function accepts a YouTube video ID as an argument
+    and returns the following:
+        - The title of the video
+        - A list of tags associated with the video
+        - The length (in seconds) of the YouTube video
 
-    :param video_id: Used to Specify the video id of the youtube link.
-    :return: The title of the video from the youtube link.
+    :param video_id: Used to Specify the video that we want to get information about.
+    :return: A dictionary containing information about the video.
     """
-    with urllib.request.urlopen(f"https://www.youtube.com/watch?v={video_id}") as video:
-        youtube = etree.HTML(video.read())
-        video_title = youtube.xpath("//meta[@name='title']/@content")
-
-    if video_title == [""]:
-        return ""
-    video_information = f"{video_title}"
-    return "".join(video_information)
-
-
-def get_video_length_from_video_id(video_id: str) -> str:
-    """
-    The get_video_length_from_video_id function takes a video_id
-    as an argument and returns the length of the video.
-
-    :param youtube_link: Used to Specify the youtube link.
-    :return: The length of the video from the youtube link.
-    """
-    with urllib.request.urlopen(f"https://www.youtube.com/watch?v={video_id}") as video:
-        html = etree.HTML(video.read())
-
-        parsed_html = BeautifulSoup(html, features="lxml")
-
-        print(parsed_html.title)  # Needs to be fixed
-
-        return str(parsed_html.body.find("span", attrs={"class": "ytp-time-duration"}))
-
-
-def get_video_thumbnail_url_from_video_id(video_id) -> str:
-    """
-    The get_video_thumbnail_url_from_video_id function accepts a video id
-    and returns the url of the video's thumbnail.
-
-    :param video_id: Used to Specify the video id.
-    :return: The url of the video thumbnail.
-    """
-    with urllib.request.urlopen(
-        f"img.youtube.com/vi/{video_id}/maxresdefault.jpg"
-    ) as thumbnail:
-        return etree.HTML(thumbnail.read())
+    try:
+        yt = YouTube(f"http://youtube.com/watch?v={video_id}")
+    except exceptions.PytubeError:
+        logging.warning(f"Video id {video_id} is unavailable.")
+        return {}
+    if yt != {}:
+        return {
+            "title": yt.title,
+            "author": yt.author,
+            "channel_id": yt.channel_id,
+            "channel_url": yt.channel_url,
+            "thumbnail_url": yt.thumbnail_url,
+            "description": yt.description,
+            "keywords": yt.keywords,
+            "length": yt.length,
+            "publish_date": yt.publish_date,
+            "rating": yt.rating,
+            "views": yt.views,
+        }
