e58739b05d1d328fc048e31f3cdde6d4f8804a50
christianhofmanncodes <christianhofmann@mailbox.org>
2022-11-04 11:11:05 +0100

    Bump up version from 0.1.0 to 0.2.0 and release date to November

--- src/main/python/settings/settings.py
+++ src/main/python/settings/settings.py
@@ -1,8 +1,8 @@
 """module settings.settings"""
 
-APP_VERSION = "0.1.0"
+APP_VERSION = "0.2.0"
 APP_ICON = "icon/youtube-play.icns"
-RELEASE_DATE = "Oct 30 2022"
+RELEASE_DATE = "Nov 30 2022"
 RECENT_FILES_STRING = "Clear recent files"
 SETTING_FILE_LOCATION = "config/settings.config"
 DEFAULT_SETTINGS_FILE_LOCATION = "config/settings_default.config"
