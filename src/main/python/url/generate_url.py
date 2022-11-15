6efeef4b5882f29b0b3e608634c98fe936dacca0
christianhofmanncodes <christianhofmann@mailbox.org>
2022-11-15 13:42:23 +0100

    Fix security issue with urllib, refactor code

--- src/main/python/url/generate_url.py
+++ src/main/python/url/generate_url.py
@@ -17,16 +17,16 @@ def playlist_url(self, video_ids_url: str) -> str:
     :param video_ids_url:str: Used to Get the video ids from the user input.
     :return: The url of the playlist that is generated from the video ids url.
     """
+    if not video_ids_url.lower().startswith("https"):
+        raise ValueError from None
     try:
         ctx = ssl._create_default_https_context()
         with request.urlopen(video_ids_url, context=ctx) as response:
             playlist_link = response.geturl()
             playlist_link = playlist_link.split("list=")[1]
 
-        return (
-            f"https://www.youtube.com/playlist?list={playlist_link}"
-            + "&disable_polymer=true"
-        )
+        return f"https://www.youtube.com/playlist?list={playlist_link}&disable_polymer=true"
+
     except (error.URLError, IndexError, UnicodeEncodeError):
         creating_url.show_error_creating_url_dialog(self)
         logging.warning("An error occurred while generating the playlist URL!")
